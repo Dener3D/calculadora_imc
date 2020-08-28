ec1ad6134b6db2bd5ca91e4ec1348fcab309a7c1
Dener3D <dener3d.designer@gmail.com>
2020-08-28 13:46:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { useState } from 'react';
+import GlobalStyle from './global';
+import { Container, Content, Info, EntradaContainer, Entrada, Button, Mensagem } from './styles';
+import logo from './assets/logo.png';
+
+function App() {
+
+ const [total, setTotal] = useState("");
+ const [mensagem, setMensagem] = useState("");
+ const [mainColor, setColor] = useState("red");
+ var y = 0.0;
+ var z = 0.0;
+
+
+  function limpar () {
+    setTotal("");
+    setMensagem("");
+    document.querySelectorAll('.altura')[0].value = "";
+    document.querySelectorAll('.peso')[0].value = "";
+  }
+
+  function calcular(resultado){
+    y = parseFloat(document.querySelectorAll('.altura')[0].value);
+    z = parseFloat(document.querySelectorAll('.peso')[0].value);
+    resultado = z / (y * y);
+    setTotal("Seu IMC é de: " + resultado.toFixed(2));
+
+    console.log(y);
+
+    if (document.querySelectorAll('.altura')[0].value === "" || document.querySelectorAll('.peso')[0].value === ""){
+      setTotal("Preencha todos os campos!");
+    }
+    
+    else{
+
+    if (resultado.toFixed(2) <= 18.5){
+      setMensagem("Você está com o peso abaixo do normal!");
+      setColor("red");
+    }
+
+    else if (resultado.toFixed(2) >= 18.5 && resultado.toFixed(2) <= 24.9){
+      setMensagem("Você está com o peso normal");
+      setColor("green");
+    }
+    
+
+    else {
+      setMensagem("Você está com o peso acima do normal!");
+      setColor("red");
+    }
+  }
+  }
+
+  return <Container>
+
+    
+    <Content>
+
+  <img src={logo} alt="logo-image"></img>
+
+  <EntradaContainer>
+    <label>Altura:</label>
+    <Entrada type="number" className="altura" step={0.01}/>
+  </EntradaContainer> 
+
+  <EntradaContainer>
+    <label>Peso:</label>
+    <Entrada type="number" className="peso"/>
+  </EntradaContainer> 
+
+  <EntradaContainer>
+    <Button mainColor={"#40b884"} type="button" onClick={calcular}>CALCULAR</Button>
+    <Button mainColor={"#259fcc"} type="button" onClick={limpar}>LIMPAR</Button>
+  </EntradaContainer> 
+
+    <h2>{total}</h2>
+    <Mensagem mainColor={mainColor} > {mensagem}</Mensagem>
+    </Content>
+    <Info>
+      <h3>MENOR QUE 18,5        -     MAGREZA</h3>
+      <h3>ENTRE 18,5 E 24,9     -     NORMAL</h3>
+      <h3>ENTRE 25,0 E 29,9     -     SOBREPESO</h3>
+      <h3>ENTRE 30,0 E 39,9     -     OBESIDADE</h3>
+      <h3>MAIOR QUE 40,0        -     OBESIDADE GRAVE</h3>
+
+    </Info>
+    <GlobalStyle />
+  </Container>
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Info = styled.div`
+    width: 100%;
+    max-width: 400px;
+    margin: 10 0 0 0;
+    background: #FFF;
+    border-radius: 4px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+    font-size: 10px;
+
+    h3 {
+        margin: 10px;
+        background-color: #d1f8ff;
+        padding: 10px;
+        text-align: center;
+        border-radius: 10px;
+    }
+
+`
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 400px;
+    margin: 30px;
+    background: #FFF;
+    border-radius: 4px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img{
+        height: 70px;
+        margin-bottom: 23px;
+    }
+
+    h4 {
+        margin-top: 10px;
+        
+    }
+`;
+
+export const EntradaContainer = styled.form`
+    height: 30px;
+    width: 100%;
+    font-size: 14px;
+    margin-bottom: 30px;
+    text-align: center;
+`;
+
+export const Entrada = styled.input`
+    background-color: #d1f8ff;
+    height: 30px;
+    width: 70%;
+    font-size: 20px;
+    margin-bottom: 10px;
+    margin-left: 8px;
+    font-size: 20px;
+    text-align: center;
+    border: none;
+`;
+
+export const Button = styled.button`
+    color: #FFF;
+    height: 40px;
+    width: 40%;
+    border: none;
+    border-radius: 2px;
+    padding: 10px;
+    font-weight: bold;
+    margin-left: 10px;
+    background: ${props => `${props.mainColor}`}
+    
+    
+`;
+
+export const Mensagem = styled.h4`
+    margin-top: 10px;
+    color: ${props => `${props.mainColor}`}
+    
+    
+`;
+
+
